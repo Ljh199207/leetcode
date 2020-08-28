b2d3bb78cfe72af7819ce981140d5084581084a5
Ljh199207 <1054934778@qq.com>
2020-08-28 14:35:19 +0800

    1

---
+++ springcloud-ljh/springboot-elasearch/src/main/java/com/example/springbootelasearch/elas/controller/PmsBrandController.java
@@ -0,0 +1,20 @@
+package com.example.springbootelasearch.elas.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * <p>
+ * 品牌表 前端控制器
+ * </p>
+ *
+ * @author ljh
+ * @since 2020-08-21
+ */
+@Controller
+@RequestMapping("/test/pmsBrand")
+public class PmsBrandController {
+
+}
---
+++ springcloud-ljh/springboot-elasearch/src/main/java/com/example/springbootelasearch/elas/service/PmsBrandService.java
@@ -0,0 +1,16 @@
+package com.example.springbootelasearch.elas.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.example.springbootelasearch.elas.entity.PmsBrand;
+
+/**
+ * <p>
+ * 品牌表 服务类
+ * </p>
+ *
+ * @author ljh
+ * @since 2020-08-21
+ */
+public interface PmsBrandService extends IService<PmsBrand> {
+
+}
